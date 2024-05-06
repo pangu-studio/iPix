1847cb7ba92af200b2446474b4b99c9a86207e87
liwei <contact@liwei.email>
2024-05-06 22:31:10 +0800

    add logger for flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -9,24 +8,22 @@ import 'package:window_manager/window_manager.dart';
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
+import 'package:logger/logger.dart';
 
-Future setupLogger() async {
-  setupLogStream().listen((msg) {
-    var level = msg.logLevel.toString().replaceAll("Level.", "");
-    var dt = DateTime.now();
-    var fdt = DateFormat('yyyy-MM-dd HH:mm:ss').format(dt);
-    // This should use a logging framework in real applications
-    // and this will not be showed on mobile devices
-    // ignore: avoid_print
-    print("$fdt [${level.padRight(5)}] [${msg.lbl.padRight(12)}]: ${msg.msg}");
-  });
-}
+var logger = Logger();
 
 Future<void> main() async {
-  await RustLib.init();
-  await setupLogger();
-  //needed to ensure binding was initialized
+  // Initialize the Rust library
+  RustLib.init();
+
+  // Desktop platform brige flutter logger for debugging
+  if (Platform.isMacOS || Platform.isWindows || Platform.isLinux) {
+    await setupLogger();
+  }
+  // Ensure that the WidgetsBinding is initialized before calling flutter api
   WidgetsFlutterBinding.ensureInitialized();
+  // init database
+  await setupDatabase();
 
   if (Platform.isMacOS) {
     //macOS相关代码
@@ -44,26 +41,12 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-// Future<void> initLib2() async {
-//   try {
-//     String dir = Directory.current.path;
-
-//     await initLib(path: dir);
-//   } catch (e) {
-//     log("error: $e");
-//   }
-// }
-
 class _HomePageState extends State<HomePage> {
   // const MyApp({super.key});
   var ip = '';
 
   @override
   void initState() {
-    // fetchIp();
-    // String dir = Directory.current.path;
-
-    // initLib(path: dir);
     super.initState();
   }
 
@@ -101,39 +84,63 @@ class _HomePageState extends State<HomePage> {
 
 Future<String> getIp() async {
   try {
-    // var dbPath = await getDatabasesPath();
-    // log(dbPath);
-    // var appPath = await getApplicationDocumentsDirectory();
-    // log(appPath.path);
-    // if (Platform.isAndroid) {
-    //   var appPath = await getExternalStorageDirectory();
-    //   log(appPath.toString());
-    // }
+    var ip = await simpleUseAsyncSpawn(arg: "test");
+    logger.d("ip: $ip");
+    return ip;
+  } catch (e) {
+    logger.e("error: $e");
+  }
+  return '';
+}
+
+// setup logger for desktop platform
+Future setupLogger() async {
+  setupLogStream().listen((msg) {
+    var level = msg.logLevel.toString().replaceAll("Level.", "");
+    var dt = DateTime.now();
+    var fdt = DateFormat('yyyy-MM-dd HH:mm:ss').format(dt);
+    // This should use a logging framework in real applications
+    // and this will not be showed on mobile devices
+    // ignore: avoid_print
+    print("$fdt [${level.padRight(5)}] [${msg.lbl.padRight(12)}]: ${msg.msg}");
+  });
+}
+
+Future<void> setupDatabase() async {
+  try {
+    // init database
+    // db_path refering sqflite getDatabasesPath
     String dbPath;
     if (Platform.isAndroid) {
-      var dir = await getApplicationDocumentsDirectory();
+      // for Android /data/user/0/**/databases/data.db
+      // getApplicationDocumentsDirectory() return /data/user/0/**/app_flutter
+      // replace /app_flutter with /databases
+      var docDir = await getApplicationDocumentsDirectory();
+      var dbDirPath = join(docDir.parent.path, "databases");
+      Directory dbDir = Directory(dbDirPath);
 
-      var dbPath2 = join(dir.parent.path, "databases");
-      Directory db = Directory(dbPath2);
-      if (!db.existsSync()) {
-        dir.createSync(recursive: true);
+      // create database directory if not exists
+      if (!dbDir.existsSync()) {
+        dbDir.createSync(recursive: true);
       }
-      dbPath = db.path;
+      dbPath = dbDir.path;
     } else {
+      // for iOS /var/mobile/Containers/Data/Application/**/Documents/data.db
       var dir = await getApplicationDocumentsDirectory();
       dbPath = dir.path;
+    }
+    logger.d("init Lib for sqlite: $dbPath");
+    if (dbPath.isEmpty) {
+      logger.e("dbPath is null");
+      return;
     }
     await initLib(path: dbPath);
     Directory db = Directory(dbPath);
     db.listSync().forEach((element) {
-      log("files: ${element.path}");
+      logger.d("list -> files: ${element.path}");
     });
-    print('path: $dbPath\n');
-    var ip = await simpleUseAsyncSpawn(arg: dbPath);
-
-    return ip;
+    logger.d('path: $dbPath\n');
   } catch (e) {
-    print(e);
+    logger.e("error: $e");
   }
-  return '';
 }
